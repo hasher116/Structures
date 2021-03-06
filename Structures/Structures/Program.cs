1cd5301c60a616c6d97d2f63b6f738b610fba6ef
hasher116 <andrei13081996@mail.ru>
2021-03-06 06:32:27 +0300

    Initial commit

---
+++ Structures/Structures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User Andrew = new User(24, "Андрей");
+            Andrew.GetInfo();
+
+            User Vasya;
+            Vasya.age = 25;
+            Vasya.name = "Вася";
+            Vasya.GetInfo();
+
+            User Igor = new User();
+            Igor.GetInfo();
+        }
+    }
+    struct User
+    {
+        public int age;
+        public string name;
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"age = {age}\nname = {name}");
+        }
+
+        public User(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+    }
+}
